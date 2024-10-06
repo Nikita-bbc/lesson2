9d69eb098b69be83cc95247f7d0cae41169226b0
Nikita-bbc <nikn08.58@gmail.com>
2024-10-07 00:23:52 +0300

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -1,38 +1,37 @@
 class Vehicle:
-    __color_variants = ['green', 'blue', 'white', 'red']
+    __COLOR_VARIANTS = ['green', 'blue', 'white', 'red']
     def __init__(self, owner, __model, __engine_power, __color):
         self.owner = owner
-        self.model = __model
-        self.engine_power = __engine_power
-        self.color = __color
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
 
     def get_model(self):
-        return f'Марка машины: {self.model}'
+        return f'Марка машины: {self.__model}'
     def get_horsepower(self):
-        return f'Мощность машины: {self.engine_power}'
+        return f'Мощность машины: {self.__engine_power}'
     def get_color(self):
-        return f'Цвет: {self.color}'
+        return f'Цвет: {self.__color}'
     def print_info(self):
         print(self.get_model())
         print(self.get_horsepower())
         print(self.get_color())
         print(f'Владелец: {self.owner}')
     def set_color(self, new_color):
-        if new_color.lower() in self.__color_variants:
-            self.color = new_color
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
         else:
-            print('Цвет нельзя изменить')
+            print(f'Цвет нельзя изменить на {new_color}')
 
 
 class Sedan(Vehicle):
-    __passengers_limit = 5
+    __PASSENGERS_LIMIT = 5
 
 
 
 
 car = Sedan('Roma', 'Nissan', 500, 'orange')
 car.print_info()
-car.set_color('White')
-car.set_color('BLacK')
+car.set_color('black')
 car.owner = 'Artem'
 car.print_info()
