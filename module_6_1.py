6bf19a0426244be33c4f846c0ccb1ee8b534f709
Nikita-bbc <nikn08.58@gmail.com>
2024-10-08 22:08:37 +0300

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -24,7 +24,7 @@ class Mammal(Animal):
 
 
 class Predator(Animal):
-   pass
+    pass
 
 
 class Flower(Plant):
@@ -45,42 +45,9 @@ print(p1.name)
 
 print(a1.alive)
 print(a2.fed)
+
 a1.eat(p1)
 a2.eat(p2)
-print(a1.alive)
-print(a2.fed)
-
-
-class Predator(Animal):
-    def eat(self, food):
-        if isinstance(food, Flower):
-            self.alive = False
-            print(f'{self.name} не стал есть {food.name}')
-        else:
-            self.fed = True
-            print(f'{self.name} съел {food.name}')
-
-
-class Flower(Plant):
-    edible = False
-
-
-class Fruit(Plant):
-    edible = True
-
-
-a1 = Predator('Волк с Уолл-Стрит')
-a2 = Mammal('Хатико')
-p1 = Flower('Цветик семицветик')
-p2 = Fruit('Заводной апельсин')
-
-print(a1.name)
-print(p1.name)
 
 print(a1.alive)
 print(a2.fed)
-a1.eat(p1)
-a2.eat(p2)
-print(a1.alive)
-print(a2.fed)
-
